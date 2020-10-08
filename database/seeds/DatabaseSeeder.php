1a31af63982b09dabc4f2b264fb1cb238e15f0df
Gaston Estevez <Gaston.Estevez@dh.edu.ar>
2020-10-07 23:25:14 -0300

    agrego timestamps

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Carbon\Carbon;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,9 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Tato',
+            'nombre' => 'Tato',
+            'apellido' => 'Estevez',
+            'numero' => '03-03-456',
+            'foto' => '#',
+            'is_admin' => true,
             'email' => 'dakota@gmail.com',
             'password' => bcrypt('asdfasdf'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
 }
