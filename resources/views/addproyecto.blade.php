5bbaced3a418e4604c249b19f326b8ed1311df76
HP Rodriguez <taten.usa@gmail.com>
2020-10-20 19:56:01 -0300

    Trabajando en vista addproyecto

--- resources/views/addproyecto.blade.php
+++ resources/views/addproyecto.blade.php
@@ -16,8 +16,10 @@
     <script type="text/javascript">WebFont.load({  google: {    families: ["Varela:400","Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic","Oswald:200,300,400,500,600,700","Karla:regular,700"]  }});</script>
     <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+    <script src="/js/uikit.js" charset="utf-8"></script>
     <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
     <link href="/images/webclip.png" rel="apple-touch-icon">
+    <link rel="stylesheet" href="/css/uikit.css">
     <!-- REPLACE ↓↓ -->
     <!--  Temporary Memberstack Code  -->
     <script src="https://api.memberstack.io/static/memberstack.js" data-memberstack-id="492fe7dc98e13f13d8933202d4c9b994"></script>
@@ -69,8 +71,8 @@
               @endforeach
             </select>
           </div>
-          <div style="display: flex; flex-direction: column; justify-self: left;">
-            <label for="destacado" class="field-label-30">Destacar</label>
+          <div>
+            <label style="text-align:center;" for="destacado" class="field-label-30">Destacar</label>
             <input type="checkbox" class="text-field-15 w-input" maxlength="256" name="destacado" data-name="porcentaje" placeholder="" id="destacado" required>
           </div>
           <div id="imagenes" class="div-block-404">
@@ -94,8 +96,8 @@
 
           <br><br>
 
-          <label for="referente" class="field-label-30">Referentes</label>
-          <div id="w-node-9b4e0133bb7d-dab82f42" class="div-block-1814">
+          <label style="text-align:center;" for="referente" class="field-label-30">Referentes</label>
+          {{-- <div id="w-node-9b4e0133bb7d-dab82f42" class="div-block-1814">
             <div>
               <label for="referente" class="field-label-30">Nombre</label>
               <input type="text" maxlength="256" data-name="" name="referente" id="referente" class="text-field-15 w-input">
@@ -115,9 +117,71 @@
                 <input id="foto_referente" type="file" name="foto_referente" data-wait="Please wait..." class="submit-button-15 w-button" multiple>
               </div>
             </div>
+          </div> --}}
+
+          <div class="uk-overflow-auto">
+            <table class="uk-table uk-table-small uk-table-divider">
+              <thead>
+                <tr id="labels" >
+                  <th class="field-label-30">Nombre</th>
+                  <th class="field-label-30">Tipo de Referente</th>
+                  <th class="field-label-30">Cargar Foto</th>
+                </tr>
+              </thead>
+              <tbody id="table_unidad_body">
+                <tr>
+                  <th>
+                    <input type="text" maxlength="256" data-name="" name="referente" id="referente" class="text-field-15 w-input">
+                  </th>
+                  <th>
+                    <select id="tipo_de_referente" name="tipo_de_referente" class="w-select">
+                      <option value="asd">Seleccione un tipo</option>
+                      @foreach ($tipo_de_referentes as $tipo_de_referente)
+                        <option value="{{$tipo_de_referente->id}}">{{$tipo_de_referente->tipo}}</option>
+                      @endforeach
+                    </select>
+                  </th>
+                  <th>
+                    <input id="foto_referente" type="file" name="foto_referente" data-wait="Please wait..." class="submit-button-15 w-button" style="transform: scale(0.60);">
+                  </th>
+                  <th>
+                    <button style="margin: 0px 0px 11px;" type="button" name="button">X</button>
+                  </th>
+                </tr>
+
+                <tr>
+                  <th>
+                    <input type="text" maxlength="256" data-name="" name="referente" id="referente" class="text-field-15 w-input">
+                  </th>
+                  <th>
+                    <select id="tipo_de_referente" name="tipo_de_referente" class="w-select">
+                      <option value="asd">Seleccione un tipo</option>
+                      @foreach ($tipo_de_referentes as $tipo_de_referente)
+                        <option value="{{$tipo_de_referente->id}}">{{$tipo_de_referente->tipo}}</option>
+                      @endforeach
+                    </select>
+                  </th>
+                  <th>
+                    <input id="foto_referente" type="file" name="foto_referente" data-wait="Please wait..." class="submit-button-15 w-button" style="transform: scale(0.60);">
+                  </th>
+                  <th>
+                    <button style="margin: 0px 0px 11px;" type="button" name="button">X</button>
+                  </th>
+                </tr>
+              </tbody>
+            </table>
+
+            <br>
+
+            <div style="width:50%;"id="w-node-3a3f3b505af0-dab82f42" class="div-block-1808">
+              <button id="agregar_referente" data-wait="Please wait..." class="submit-button-14 w-button">Agregar Referente</button
+            </div>
           </div>
+
+          <br> <br>
+
           <div id="w-node-3a3f3b505af0-dab82f42" class="div-block-1808">
-            <input type="submit" value="Cargar" data-wait="Please wait..." class="submit-button-14 w-button">
+            <input type="submit" value="Cargar Proyecto" data-wait="Please wait..." class="submit-button-14 w-button">
           </div>
         </form>
 
@@ -158,6 +222,49 @@
             } catch(e) {}
           }
       })
+    </script>
+
+    <script type="text/javascript">
+
+    // agregar unidades
+    var agregar_unidad_button = document.getElementById('agregar_unidad');
+    var table_unidad_body = document.getElementById('table_unidad_body');
+    var labels = document.getElementById('labels');
+    var cantidad_unidades = 0;
+    var id_unidad = 1;
+    agregar_unidad_button.addEventListener('click',function(){
+      var table_tr = document.createElement('tr');
+      var array = ["[piso]","[m2]","[ambientes]","[baños]","[toilettes]","[dormitorios]","[precio]"]
+      for (var i = 0; i < 7; i++) {
+        var table_th = document.createElement('th');
+        var table_input = document.createElement('input');
+        var query = "unidad["+id_unidad+"]"+array[i];
+        table_input.setAttribute("style","width:70%; margin-top: 10px;")
+        table_input.setAttribute("type","number");
+        table_input.setAttribute('name',query);
+        labels.removeAttribute('hidden');
+        table_th.appendChild(table_input);
+        table_tr.appendChild(table_th);
+        table_unidad_body.appendChild(table_tr);
+      }
+      var table_th = document.createElement('th');
+      var table_detele = document.createElement('button');
+      table_detele.innerHTML = "X";
+      table_th.appendChild(table_detele)
+      table_th.style = "padding-top: 11px;"
+      table_tr.appendChild(table_th)
+      table_unidad_body.appendChild(table_tr);
+      id_unidad++;
+      cantidad_unidades++;
+      table_detele.addEventListener('click',function(){
+        cantidad_unidades--;
+        this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+        if (cantidad_unidades===0) {
+          labels.setAttribute('hidden',true);
+        }
+      });
+    });
+  });
     </script>
   </body>
 </html>
