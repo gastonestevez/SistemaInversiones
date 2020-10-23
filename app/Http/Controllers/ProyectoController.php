7fabf974b70cf15981f95e8f0e97c9594969cfe9
Gaston Estevez <Gaston.Estevez@dh.edu.ar>
2020-10-22 21:31:28 -0300

    chausi

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -15,7 +15,6 @@ class ProyectoController extends Controller
 
   public function new()
   {
-    
     $localidades = Localidad::all();
     $asesores = Asesor::all();
     $tipo_de_referentes = Tipo_de_referente::all();
